625eec7fce914ed2f47b77b8ace5da236da82515
Gyusik Jeong <sinoroo@naver.com>
2022-10-22 15:11:47 +0900

    멤버 조회 기능 추가

--- src/main/java/com/sinoroo/demo1/controller/MemberController.java
+++ src/main/java/com/sinoroo/demo1/controller/MemberController.java
@@ -3,8 +3,11 @@ package com.sinoroo.demo1.controller;
 import com.sinoroo.demo1.service.MemberService;
 import com.sinoroo.demo1.domain.Member;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -30,5 +33,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
