f5402540e930cf275de9bd1f62572ee8ab360540
Gyusik Jeong <sinoroo@naver.com>
2022-10-22 10:09:17 +0900

    회원가입까지 완료

--- src/main/java/com/sinoroo/demo1/controller/MemberController.java
+++ src/main/java/com/sinoroo/demo1/controller/MemberController.java
@@ -1,10 +1,12 @@
 package com.sinoroo.demo1.controller;
 
 import com.sinoroo.demo1.service.MemberService;
+import com.sinoroo.demo1.domain.Member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -14,5 +16,19 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
